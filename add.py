d56681f3dd1cdcec16f816b05812943aed0f6137
V N S <2200090003@kluniversity.in>
2024-01-11 14:04:54 +0530

    this is a version 1 file

---
+++ add.py
@@ -0,0 +1,12 @@
+this is a c prog python file
+int sum1,sum2,sum
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C:
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[;
+scanf &sum1,&sum2
+printf enter the values of sum1 and sum2
+sum=sum1+sum2
+printf sum
+
+[A
+
+
